0f56afd91bf662d8634a819f11628521c9cd5bc4
Madrigales <d.madrigales26@gmail.com>
2018-03-13 20:36:15 -0600

    OperacionesConJAVA

---
+++ OperacionesConJava/src/OperacionesConJava.java
@@ -0,0 +1,17 @@
+
+public class OperacionesConJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       JavaOperaciones JOperaciones = new JavaOperaciones();
+       JOperaciones.Mensaje1();
+       JOperaciones.Mensaje1_2();
+       JOperaciones.Resta_Multiplicacion();
+       JOperaciones.Suma();
+       JOperaciones.RetornarNumeros();
+      
+    }
+    
+}
